191e0731f8a2b066481777ca12886b37fb043f97
Wang, Dawei  OA/STS-TWC <wangdae@schaeffler.com>
2026-09-09 13:04:32 +0800

    perf: reorganize train code
    1. prioritize picking up resources over transferring resources from container.
    2. add cache to the source and dest container logic. Source container is optional, dest container is mandatory.
    3. Use resource constant instead of always RESOURCE_ENERGY to indicate state shift from picking up to delivery.

--- src/modules/role/role.train.ts
+++ src/modules/role/role.train.ts
@@ -4,128 +4,104 @@
  * */
 const roleTrain = {
     run: (creep: Creep) => {
-        if (creep.memory.transporting && creep.store.getUsedCapacity(RESOURCE_ENERGY) === 0) {
+        const resourceConstant = creep.memory.mineralType ?? RESOURCE_ENERGY
+        if (creep.memory.transporting && creep.store.getUsedCapacity(resourceConstant) === 0) {
             creep.memory.transporting = false;
         }
-        if (!creep.memory.transporting && creep.store.getFreeCapacity(RESOURCE_ENERGY) === 0) {
+        if (!creep.memory.transporting && creep.store.getFreeCapacity(resourceConstant) === 0) {
             creep.memory.transporting = true;
         }
-        let resourceConstant: ResourceConstant;
-        if (creep.memory.mineralType) {
-            resourceConstant = creep.memory.mineralType
-        } else {
-            resourceConstant = RESOURCE_ENERGY;
+        creep.memory.cache_max_duration ??= 50;
+        creep.memory.cache_duration ??= 51;
+        if (!creep.memory.destFlagName || !creep.memory.srcFlagName) {
+            console.log("Check " + creep.name + "'s memory, it does not have destFlagName or srcFlagName")
+            return;
         }
+        const destFlag = Game.flags[creep.memory.destFlagName];
+        const srcFlag = Game.flags[creep.memory.srcFlagName];
+        if (creep.memory.cache_duration > creep.memory.cache_max_duration) {
+            // Deal with dest container MUST HAVE
+            const dest_containers = destFlag.pos.findInRange(FIND_STRUCTURES, 0, {
+                filter: object => object.structureType === STRUCTURE_CONTAINER ||
+                    object.structureType === STRUCTURE_STORAGE
+            });
+            const dest_links = resourceConstant === RESOURCE_ENERGY ? destFlag.pos.findInRange(FIND_STRUCTURES, 0, {
+                filter: s => s.structureType === STRUCTURE_LINK
+            }) : undefined;
+            const dest_container = dest_containers.pop();
+            const dest_link = dest_links?.pop();
+            if (dest_container) {
+                creep.memory.cache_dest_container_id = dest_container.id as Id<StructureContainer> | Id<StructureStorage>;
+            } else if (dest_link) {
+                creep.memory.cache_dest_container_id = dest_link.id as Id<StructureLink>;
+            } else {
+                console.log("ERROR! The destFlag " + destFlag.name + " does not have a container or link");
+                return;
+            }
+
+            // Deal with src container OPTIONALLY HAVE
+            const src_containers = srcFlag.pos.findInRange(FIND_STRUCTURES, 0, {
+                filter: object => object.structureType === STRUCTURE_CONTAINER ||
+                    object.structureType === STRUCTURE_STORAGE
+            });
+            const src_container = src_containers.pop()
+            if (src_container) {
+                creep.memory.cache_src_container_id = src_container.id as Id<StructureContainer> | Id<StructureStorage>;
+            } else {
+                console.log("Warning! The srcFlag " + srcFlag.name + " does not have a container");
+                creep.memory.cache_src_container_id = undefined;
+            }
+            creep.memory.cache_duration = 0;
+
+        } else {
+            creep.memory.cache_duration++;
+        }
+
         if (creep.memory.transporting) {
-            // const structure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-            //     filter: s => (s.structureType === STRUCTURE_SPAWN ||
-            //             s.structureType === STRUCTURE_TOWER || s.structureType === STRUCTURE_EXTENSION)
-            //         && s.store.getFreeCapacity(RESOURCE_ENERGY) > 0
-            // })
-            // if (structure) {
-            //     if (creep.transfer(structure, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-            //         creep.moveTo(structure);
-            //     }
-            // } else {
-            if (creep.memory.destFlagName) {
-                const destFlag = Game.flags[creep.memory.destFlagName];
-                if (destFlag) {
-                    if (!creep.pos.isNearTo(destFlag)) {
-                        creep.moveTo(destFlag);
-                    } else {
-                        const containers = destFlag.pos.findInRange(FIND_STRUCTURES, 0, {
-                            filter: object => object.structureType === STRUCTURE_CONTAINER ||
-                                object.structureType === STRUCTURE_STORAGE
-                        })
-                        const links = destFlag.pos.findInRange(FIND_STRUCTURES, 0, {
-                            filter: s => s.structureType === STRUCTURE_LINK
-                        })
-                        const container = containers.pop()
-                        const link = links.pop()
-                        if (container) {
-                            if (creep.transfer(container, resourceConstant) !== OK) {
-                                creep.say("container full")
-                                // creep.drop(RESOURCE_ENERGY);
-                            }
-                        } else {
-                            if (resourceConstant === RESOURCE_ENERGY) {
-                                if (link) {
-                                    if (creep.transfer(link, resourceConstant) !== OK) {
-                                        creep.say("link full")
-                                    }
-                                }
-                            } else {
-                                console.log(creep.name + " cannot find container. This is a mineral train")
-                            }
+            if (!creep.pos.isNearTo(destFlag)) {
+                creep.moveTo(destFlag);
+            } else {
+                const container = Game.getObjectById(creep.memory.cache_dest_container_id!)
+                if (container) {
+                    if (creep.transfer(container, resourceConstant) !== OK) {
+                        creep.say("container full")
+                    }
+                } else {
+                    creep.memory.cache_duration = creep.memory.cache_max_duration + 1;
+                }
+            }
+        } else {// pick up resource
+            if (srcFlag.room && srcFlag.room.name === creep.room.name) {
+                const resources = srcFlag.pos.findInRange(FIND_DROPPED_RESOURCES, 0, {
+                    filter: r => r.amount >= creep.store.getFreeCapacity(resourceConstant)
+                });
+                const container = creep.memory.cache_src_container_id ? Game.getObjectById(creep.memory.cache_src_container_id) : undefined
+                if (resources.length > 0 || (container &&
+                    container.store.getUsedCapacity(resourceConstant) >= creep.store.getFreeCapacity(resourceConstant))) {
+                    const resource = resources.pop();
+                    if (resource) {
+                        if (creep.pickup(resource) === ERR_NOT_IN_RANGE) {
+                            creep.moveTo(resource);
+                        }
+                    } else if (container) {
+                        if (creep.withdraw(container, resourceConstant) === ERR_NOT_IN_RANGE) {
+                            creep.moveTo(container)
+                            // console.log("Cannot withdraw from container")
                         }
                     }
                 } else {
-                    console.log("Check " + creep.name + "'s memory, the flag cannot be found")
+                    creep.gotoIdleFlag()
+                    if (resourceConstant !== RESOURCE_ENERGY) {
+                        const minerals = creep.room.find(FIND_MINERALS)
+                        const mineral = minerals.pop();
+                        if (!mineral?.mineralAmount) {
+                            creep.memory.respawnInformed = true
+                        }
+                    }
                 }
             } else {
-                console.log("Check " + creep.name + "' memory, it does not have destFlagName")
+                creep.moveTo(srcFlag);
             }
-            // }
-        } else {// pick up resource
-            if (creep.memory.srcFlagName) {
-                const srcFlag = Game.flags[creep.memory.srcFlagName]
-
-                // if (resources.length === 0 && containers.length === 0) {
-                //     creep.moveTo(Game.flags['Idle']);
-                // } else {
-                // const resource = resources.pop();
-                // if (resource) {
-                if (srcFlag) {
-                    if (srcFlag.room && srcFlag.room.name === creep.room.name) {
-                        const resources = srcFlag.pos.findInRange(FIND_DROPPED_RESOURCES, 0, {
-                            filter: r => r.amount >= creep.store.getFreeCapacity(resourceConstant)
-                        });
-                        const containers: StructureContainer[] = srcFlag.pos.findInRange(FIND_STRUCTURES, 0, {
-                            filter: s => (s.structureType === STRUCTURE_CONTAINER ||
-                                    s.structureType === STRUCTURE_STORAGE) &&
-                                s.store.getUsedCapacity(resourceConstant) > creep.store.getFreeCapacity(resourceConstant)
-                        })
-                        if (resources.length > 0 || containers.length > 0) {
-                            const container = containers.pop();
-                            const resource = resources.pop();
-                            if (container || resource) {
-                                if (container) {
-                                    if (creep.withdraw(container, resourceConstant) === ERR_NOT_IN_RANGE) {
-                                        creep.moveTo(container)
-                                        // console.log("Cannot withdraw from container")
-                                    }
-                                } else if (resource) {
-                                    if (creep.pickup(resource) === ERR_NOT_IN_RANGE) {
-                                        creep.moveTo(resource);
-                                    }
-                                }
-                            } /*else {
-                                creep.gotoIdleFlag();
-                            }*/
-                        } else {
-                            creep.gotoIdleFlag()
-                            if (resourceConstant !== RESOURCE_ENERGY) {
-                                const minerals = creep.room.find(FIND_MINERALS)
-                                const mineral = minerals.pop();
-                                if (!mineral?.mineralAmount) {
-                                    creep.memory.respawnInformed = true
-                                }
-                            }
-                        }
-                    } else {
-                        creep.moveTo(srcFlag);
-                    }
-                } else {
-                    console.log("There is no flag found, check " + creep.name + "'s memory")
-                }
-                // } else {
-                //     console.error("This should not happen!")
-                // }
-
-                // }
-            }
-
-
         }
     }
 }
