96ed915b63c8c204878ff5dfb317bae1fa480179
Wang, Dawei  OA/STS-TWC <wangdae@schaeffler.com>
2026-09-23 16:07:14 +0800

    feat: add more types in types.ts

--- src/domain/types.ts
+++ src/domain/types.ts
@@ -22,6 +22,22 @@ type RemoteStage =
     | "underAttack"
     | "suspended"
 
+type ExpansionStage =
+    "idle"
+    | "scouting"
+    | "selected"
+    | "claiming"
+    | "lowLevel"
+    | "bootstrapping"
+    | "complete"
+
+
+type SerializedPosition = {
+    x: number;
+    y: number;
+    roomName: string;
+}
+
 interface SpawnRequest extends CreepDemand {
     createdAt: number;
     attempts: number;
@@ -32,7 +48,7 @@ type CreepAssignment =
     type: "source";
     sourceId: Id<Source>;
     containerId?: Id<StructureContainer>;
-    workPosition: RoomPosition;
+    workPosition: SerializedPosition;
 }
     | {
     type: "logistics";
@@ -51,3 +67,31 @@ type CreepAssignment =
     type: "defense";
     targetRoom: string;
 };
+
+type CreepDemand = {
+    key: string
+    role: CreepRole;
+    homeRoom: string;
+    priority: number;
+    body: BodyPartConstant[];
+    travelEstimate: number;
+}
+
+type LogisticsEndpoint = {
+    type: STRUCTURE_EXTENSION
+    | STRUCTURE_SPAWN
+    | STRUCTURE_LINK
+    | STRUCTURE_STORAGE
+    | STRUCTURE_TOWER
+    | STRUCTURE_LAB
+    | STRUCTURE_TERMINAL
+    | STRUCTURE_CONTAINER
+    | STRUCTURE_NUKER
+    | STRUCTURE_FACTORY | "no_container"
+    location: SerializedPosition;
+
+}
+
+type LogisticsJob = {
+
+}
